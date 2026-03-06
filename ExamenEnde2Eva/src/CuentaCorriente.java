21ce2673ff3f81d6bb3b3e50bd700a85df1a50ec
elmej <elmej@Kenneth>
2026-03-06 14:10:17 +0100

    java doc

--- ExamenEnde2Eva/src/CuentaCorriente.java
+++ ExamenEnde2Eva/src/CuentaCorriente.java
@@ -1,12 +1,25 @@
-
+/**
+ * Clase que extiende cuenta bancaria
+ */
 public class CuentaCorriente extends CuentaBancaria {
 	private int interes;
-
+	
+	/**
+	 * Constructor por defecto
+	 * 
+	 * @param titular
+	 * @param saldoInicial
+	 */
 	public CuentaCorriente(String titular, double saldoInicial) {
 		super(titular, saldoInicial);
 		this.interes = interes;
 	}
-
+	
+	/**
+	 * Método que calcula el interes
+	 * @param comision
+	 * @return interes
+	 */
 	public double calcularInteres(int comision) {
 		interes = comision;
 		return interes;
