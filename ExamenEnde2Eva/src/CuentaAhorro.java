576491391402443ccd73cf17b63b295bd3fc9320
elmej <elmej@Kenneth>
2026-03-06 14:06:40 +0100

    commit  javadoc

--- ExamenEnde2Eva/src/CuentaAhorro.java
+++ ExamenEnde2Eva/src/CuentaAhorro.java
@@ -1,13 +1,26 @@
-
+/**
+ * Clase que extiende a cuenta bancaria y añde un interes
+ */
 public class CuentaAhorro extends CuentaBancaria {
+	/** Variable que guarda el interes de la cuenta*/
 	private double interes;
-
+	
+	/**
+	 * Constructor por defecto
+	 * @param titular
+	 * @param saldoInicial
+	 * @param interes
+	 */
 	public CuentaAhorro(String titular, double saldoInicial,double interes) {
 		super(titular, saldoInicial);
 		this.interes=interes;
 		
 	}
 	
+	/**
+	 * Método que calcula el interes de la cuenta
+	 * @return el interes
+	 */
 	public int calcularInteres() {
 		int interesCobrado = (int) (this.saldo * interes);
 		
--- ExamenEnde2Eva/src/CuentaBancaria.java
+++ ExamenEnde2Eva/src/CuentaBancaria.java
@@ -1,8 +1,17 @@
+/**
+ * Clase principal que inicaliza la cuenta de banco
+ */
 public abstract class CuentaBancaria {
-
+	/** Variable que guarda el nombre del titular */
     protected String titular;
+    /**Variable que guarda el saldo de la persona */
     protected double saldo;
-
+    
+    /**
+     * Constructor por defecto
+     * @param titular
+     * @param saldoInicial
+     */
     public CuentaBancaria(String titular, double saldoInicial) {
         if (saldoInicial < 0) {
             throw new IllegalArgumentException("El saldo inicial no puede ser negativo");
@@ -10,20 +19,36 @@ public abstract class CuentaBancaria {
         this.titular = titular;
         this.saldo = saldoInicial;
     }
-
+    
+    /**
+     * Obtiene el nombre  del titular
+     * @return
+     */
     public String getTitular() {
         return titular;
     }
-
+    
+    /**
+     * Obtiene el saldo
+     * @return
+     */
     public double getSaldo() {
         return saldo;
     }
-
+    
+    /**
+     * Método que aumenta a el saldo una cantidad
+     * @param cantidad
+     */
     public void ingresar(double cantidad) {
 
         saldo += cantidad;
     }
-
+    
+    /**
+     * Método que resta una cantidad al saldo
+     * @param cantidad
+     */
     public void reintegrar(double cantidad) {
 
         saldo -= cantidad;
